89140b6bfce648b7929e288a7de2b9ae9b18f4f5
Per Engström <per.olov.engstrom@gmail.com>
2015-02-11 15:42:34 +0100

    Skrev algo1.m och uppdaterade .gitignore

---
+++ prestuff/algo1.m
@@ -0,0 +1,11 @@
+function motion = algo1(Tf, x0, dt)
+
+steps = floor(Tf/dt);
+motion = zeros(steps, 2);
+
+motion(1,1) = x0(1);
+motion(1,2) = x0(2);
+
+for i=2:steps
+    motion(i, :) = motion(i-1,:) + sqrt(dt)*randn(1,2);
+end
